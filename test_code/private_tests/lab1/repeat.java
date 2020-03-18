9ee9b223dad8d84edb7bfbed34fefcd5fbb38b51
yuanyehome <2252706531@qq.com>
2020-03-19 01:18:24 +0800

    variable repetition

---
+++ test_code/private_tests/lab1/repeat.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] a){
+        System.out.println(1);
+    }
+}
+
+
+class N
+{
+    public int doit()
+    {
+        N m;
+        m = new N();
+        m = new N();
+        return 1;
+    }
+}
