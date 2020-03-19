99e356a82d7c328a4a12272ae35522b971ffdba0
yuanyehome <2252706531@qq.com>
2020-03-19 16:41:30 +0800

    add a test

---
+++ test_code/private_tests/lab1/circle.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] a){
+        int p;
+        int q;
+        System.out.println(1);
+    }
+}
+
+class A extends B {
+
+}
+
+class B extends C {
+
+}
+
+class C extends A {
+
+}
