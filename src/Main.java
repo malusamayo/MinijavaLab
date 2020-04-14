573a5f772e7f46fa38b5939f35176a96cc9310fe
yuanyehome <2252706531@qq.com>
2020-04-14 19:49:37 +0800

    add visitor

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import visitor.*;
 import syntaxtree.*;
 import symbol.*;
 
-class MyVisitor extends DepthFirstVisitor {
-	public void visit(VarDeclaration n) {
-		Identifier id = (Identifier)n.f1;
-		System.out.println("VarName: " + id.f0.toString());
-		n.f0.accept(this);
-		n.f1.accept(this);
-		n.f2.accept(this);
-	}
-}
+//class MyVisitor extends DepthFirstVisitor {
+//	public void visit(VarDeclaration n) {
+//		Identifier id = (Identifier)n.f1;
+//		System.out.println("VarName: " + id.f0.toString());
+//		n.f0.accept(this);
+//		n.f1.accept(this);
+//		n.f2.accept(this);
+//	}
+//}
 
 public class Main {
 	public static void main(String[] args){
