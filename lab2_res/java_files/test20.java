24f3c0e5e9de517fa56def1921d28496b8052ce3
yuanyehome <2252706531@qq.com>
2020-04-18 12:51:39 +0800

    add test over 20 params

---
+++ lab2_res/java_files/test20.java
@@ -0,0 +1,39 @@
+class test20 {
+    public static void main (String[] a) {
+        Test t;
+        int x;
+        t = new Test();
+        x = t.run_test(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
+    }
+}
+
+class Test {
+    public int run_test(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10,
+                        int a11, int a12, int a13, int a14, int a15, int a16, int a17, int a18, int a19, int a20,
+                        int a21, int a22, int a23) {
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a7);
+        System.out.println(a8);
+        System.out.println(a9);
+        System.out.println(a10);
+        System.out.println(a11);
+        System.out.println(a12);
+        System.out.println(a13);
+        System.out.println(a14);
+        System.out.println(a15);
+        System.out.println(a16);
+        System.out.println(a17);
+        System.out.println(a18);
+        System.out.println(a19);
+        System.out.println(a20);
+        System.out.println(a21);
+        System.out.println(a22);
+        System.out.println(a23);
+        return 0;
+    }
+}
