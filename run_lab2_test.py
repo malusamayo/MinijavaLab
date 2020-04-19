4a4c893a756bbdf11825c0dbaa8a8534253e4c3a
malusamayo <773386@gmail.com>
2020-04-19 13:53:17 +0800

    add tests

--- run_lab2_test.py
+++ run_lab2_test.py
@@ -9,18 +9,18 @@ if len(sys.argv) <= 1:
 
 if sys.argv[1] == "-a" or sys.argv[1] == "-all":
     java_files = []
-    for test_file in os.listdir("lab2_res/java_files/"):
+    for test_file in os.listdir("lab2_res/correct/"):
         if test_file[-5:] == ".java":
             java_files.append(test_file)
 
     flag = True
     for java_file in java_files:
         file_name = java_file[0:-5]
-        os.system("javac lab2_res/java_files/%s" % java_file)
-        os.system("java -cp bin/production/minijava Main lab2_res/java_files/%s"
+        os.system("javac lab2_res/correct/%s" % java_file)
+        os.system("java -cp bin/production/minijava Main lab2_res/correct/%s"
                   % java_file)
-        ans1 = os.popen("java -cp lab2_res/java_files/ %s" % file_name).read()
-        ans2 = os.popen("java -jar pgi.jar < lab2_res/java_files/%s.pg" %
+        ans1 = os.popen("java -cp lab2_res/correct/ %s" % file_name).read()
+        ans2 = os.popen("java -jar pgi.jar < lab2_res/correct/%s.pg" %
                         file_name).read()
         print("test file %s: " % file_name.ljust(15, ' '), end='')
         if ans1 == ans2:
